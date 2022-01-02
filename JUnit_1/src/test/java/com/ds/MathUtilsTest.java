e54caa6dbaa7db31d06135aec7b6b96bc2e7b48c
SandeeraJ <j.sandeera@theglobalmarketaccess.com>
2022-01-02 11:05:05 +0530

    instance per class

--- JUnit_1/src/test/java/com/ds/MathUtilsTest.java
+++ JUnit_1/src/test/java/com/ds/MathUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MathUtilsTest {
     MathUtils mathUtils;
 
