875fe4372a0ee1d6d497e66a40c2a4d6f8fb3309
SandeeraJ <j.sandeera@theglobalmarketaccess.com>
2022-01-02 10:09:32 +0530

    initial

---
+++ JUnit_1/src/test/java/com/ds/MathUtilsTest.java
@@ -0,0 +1,31 @@
+package com.ds;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+    @Test
+    void testAdd() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0));
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10));
+    }
+
+
+}
