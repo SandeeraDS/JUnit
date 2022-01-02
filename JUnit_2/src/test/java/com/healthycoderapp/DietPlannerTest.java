a0b8ece085af6f96a5451b31fe0f6ed0c969b2eb
j_sandeera <j.sandeera@theglobalmarketaccess.com>
2022-01-02 16:13:15 +0530

    RepeatedTest

--- JUnit_2/src/test/java/com/healthycoderapp/DietPlannerTest.java
+++ JUnit_2/src/test/java/com/healthycoderapp/DietPlannerTest.java
@@ -1,9 +1,8 @@
 package com.healthycoderapp;
 
-import com.sun.org.apache.bcel.internal.classfile.Code;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -25,6 +24,25 @@ public class DietPlannerTest {
 
     @Test
     void should_ReturnCorrectDietPlan_When_CorrectCoder(){
+        // given
+        Coder coder = new Coder(1.82,75,26, Gender.MALE);
+        DietPlan expected = new DietPlan(2202,110,73,275);
+
+        // when
+        DietPlan actual = dietPlanner.calculateDiet(coder);
+
+        // then
+        assertAll(
+                () ->assertEquals(expected.getCalories(),actual.getCalories()),
+                () ->assertEquals(expected.getCarbohydrate(),actual.getCarbohydrate()),
+                () ->assertEquals(expected.getFat(),actual.getFat()),
+                () ->assertEquals(expected.getProtein(),actual.getProtein())
+
+        );
+    }
+
+    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+    void should_ReturnCorrectDietPlan_When_CorrectCoder_RepeatedTest(){
         // given
         Coder coder = new Coder(1.82,75,26, Gender.MALE);
         DietPlan expected = new DietPlan(2202,110,73,275);
